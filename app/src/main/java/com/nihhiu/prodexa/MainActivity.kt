505eaac8489831853deb126fce3074170ac8728a
Sérgio Gabriel <106330016+Nihhiu@users.noreply.github.com>
2025-07-27 03:09:59 +0100

    Add Custom Nav Bar + Icon change
    //FIXME: when "back" is used, the icon doesn't change

--- app/src/main/java/com/nihhiu/prodexa/MainActivity.kt
+++ app/src/main/java/com/nihhiu/prodexa/MainActivity.kt
@@ -1,19 +1,65 @@
 package com.nihhiu.prodexa
 
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.setupWithNavController
-import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+
+    private val destinationMap = mapOf(
+        R.id.nav_home to R.id.navigation_home,
+        R.id.nav_dashboard to R.id.navigation_dashboard,
+        R.id.nav_settings to R.id.navigation_settings
+    )
+
+    private val iconEmptyMap = mapOf(
+        R.id.nav_home to R.drawable.ic_house_empty,
+        R.id.nav_dashboard to R.drawable.ic_dashboard_empty,
+        R.id.nav_settings to R.drawable.ic_settings_empty
+    )
+
+    private val iconFullMap = mapOf(
+        R.id.nav_home to R.drawable.ic_house_filled,
+        R.id.nav_dashboard to R.drawable.ic_dashboard_filled,
+        R.id.nav_settings to R.drawable.ic_settings_filled
+    )
+
+    //FIXME: quando uso "back" não altera o icon selecionado
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        val navController = navHostFragment.navController
+        navController = navHostFragment.navController
 
-        findViewById<BottomNavigationView>(R.id.bottom_nav).setupWithNavController(navController)
+        destinationMap.keys.forEach { viewId ->
+            findViewById<ImageView>(viewId).apply {
+                setOnClickListener {
+                    val targetDestination = destinationMap[viewId]!!
+                    if (navController.currentDestination?.id != targetDestination) {
+                        navController.navigate(targetDestination)
+                    }
+                    updateIcons(viewId)
+                }
+            }
+        }
+
+        updateIcons(R.id.nav_home)
+    }
+
+    //TODO: Add melhor sombra
+    //TODO: Add transição de telas
+    //TODO: Add ripple effect
+    //TODO: temas
+
+    private fun updateIcons(selectedViewId: Int) {
+        iconEmptyMap.keys.forEach { viewId ->
+            val imageView = findViewById<ImageView>(viewId)
+            val iconRes = if (viewId == selectedViewId) iconFullMap[viewId] else iconEmptyMap[viewId]
+            imageView.setImageResource(iconRes!!)
+        }
     }
 }
